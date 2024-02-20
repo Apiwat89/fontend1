6f1da51053c6c9469beda7caf77599f88409b2cc
Apiwat89 <apiwat89123@gamil.com>
2024-02-20 10:03:45 +0700

    exam

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -5,6 +5,7 @@ const path = require("path");
 const app = express();
 
 const base_url = "http://node57407-apiwattest.proen.app.ruk-com.cloud";
+// const base_url = "http://localhost:3000";
 
 app.set("views", path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
